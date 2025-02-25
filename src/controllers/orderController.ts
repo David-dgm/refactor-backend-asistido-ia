81893cfa384b5f9f68629edf1a843039cc6ed3f2
Miguel A. Gómez <software.crafters.sl@gmail.com>
2025-02-25 18:19:37 +0000

    green: does not allow to complete a non-existing order

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -84,7 +84,7 @@ export const completeOrder = async (req: Request, res: Response) => {
 
     const order = await OrderModel.findById(id);
     if (!order) {
-        return res.send('Order not found to complete');
+        return res.status(400).send('Order not found to complete');
     }
 
     if (order.status !== 'CREATED') {
