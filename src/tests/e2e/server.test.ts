0d3ee6fa1f6418fee5ef5a9ae25103157834c9b0
Miguel A. Gómez <software.crafters.sl@gmail.com>
2025-02-25 18:13:53 +0000

    green: completes an order successfully

--- src/tests/e2e/server.test.ts
+++ src/tests/e2e/server.test.ts
@@ -136,6 +136,54 @@ describe('DELETE /orders/:id', () => {
     });
 })
 
+describe('POST /orders/:id/complete', () => {
+    let server: Server;
+
+    beforeAll(async () => {
+        const dbUrl = process.env.MONGODB_URI as string;
+        server = await createServer(3005, dbUrl);
+        await mongoose.connection.dropDatabase();
+    });
+
+    afterEach(async () => {
+        await mongoose.connection.dropDatabase();
+    });
+
+    afterAll(async () => {
+        server.close();
+    });
+
+    it('completes an order successfully', async () => {
+        const order = await createValidOrder(server);
+
+        const completeResponse = await request(server)
+            .post(`/orders/${order._id}/complete`);
+
+        expect(completeResponse.status).toBe(200);
+        expect(completeResponse.text).toBe(`Order with id ${order._id} completed`);
+    });
+
+    it('does not allow to complete a non-existing order', async () => {
+        const completeResponse = await request(server)
+            .post(`/orders/123/complete`);
+
+        expect(completeResponse.status).toBe(400);
+        expect(completeResponse.text).toBe('Order not found to complete');
+    });
+
+    it('does not allow to complete an order with status different than CREATED', async () => {
+        const order = await createValidOrder(server);
+        await request(server)
+            .post(`/orders/${order._id}/complete`);
+
+        const completeResponse = await request(server)
+            .post(`/orders/${order._id}/complete`);
+
+        expect(completeResponse.status).toBe(400);
+        expect(completeResponse.text).toBe(`Cannot complete an order with status: COMPLETED`);
+    });
+});
+
 async function createValidOrder(server: Server, discountCode?: string) {
     const order = {
         items: [
