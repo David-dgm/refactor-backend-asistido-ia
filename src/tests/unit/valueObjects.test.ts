85f0e2dac614f499bfac7827673f65c830ebc77b
Miguel A. Gómez <software.crafters.sl@gmail.com>
2025-03-03 18:47:09 +0000

    refactor: move

--- src/tests/unit/valueObjects.test.ts
+++ src/tests/unit/valueObjects.test.ts
@@ -1,13 +1,4 @@
-class PositiveNumber {
-    private constructor(readonly value: number) {}
-
-    static create(value: number): PositiveNumber {
-        if (value < 0) {
-            throw new Error("Negative numbers are not allowed");
-        }
-        return new PositiveNumber(value);
-    }
-}
+import {PositiveNumber} from "../../domain/valueObjects";
 
 describe("A positive number", () => {
     it("allows positive values", ()=>{
