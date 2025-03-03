8500e7b19d50759aa02e609863889b1591ef47fc
Miguel A. Gómez <software.crafters.sl@gmail.com>
2025-03-03 18:45:03 +0000

    refactor: introduce named constructor

--- src/tests/unit/valueObjects.test.ts
+++ src/tests/unit/valueObjects.test.ts
@@ -1,18 +1,21 @@
 class PositiveNumber {
-    constructor(readonly value: number) {
+    private constructor(readonly value: number) {}
+
+    static create(value: number): PositiveNumber {
         if (value < 0) {
             throw new Error("Negative numbers are not allowed");
         }
+        return new PositiveNumber(value);
     }
 }
 
 describe("A positive number", () => {
     it("allows positive values", ()=>{
-        const aPositiveNumber = new PositiveNumber(1);
+        const aPositiveNumber = PositiveNumber.create(1);
         expect(aPositiveNumber.value).toBe(1);
     });
 
     it("does not allow negative values", ()=>{
-        expect(() => new PositiveNumber(-1)).toThrowError("Negative numbers are not allowed");
+        expect(() => PositiveNumber.create(-1)).toThrowError("Negative numbers are not allowed");
     });
 });
