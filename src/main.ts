d652f748effbffbd1e1ad90ee72209da98f3cc92
Miguel A. Gómez <software.crafters.sl@gmail.com>
2025-03-11 17:22:14 +0000

    fix main

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import dotenv from "dotenv";
 import {createServer} from "./infrastructure/server";
 
-export const DB_URL = process.env.DB_URL
-export const PORT = process.env.port
-
 dotenv.config({
     path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 });
+
+export const DB_URL = process.env.DB_URL
+export const PORT = process.env.port
 
 if (!DB_URL || !PORT) {
     console.error('Missing environment variables');
     process.exit(1);
 }
 
-createServer(PORT, DB_URL);
+createServer(Number(PORT), DB_URL);
