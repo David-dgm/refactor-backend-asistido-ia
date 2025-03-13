bd5bb01ac8dcea4e215e6282d543cb5d6b82a6f8
Miguel A. Gómez <software.crafters.sl@gmail.com>
2025-03-13 17:20:16 +0000

    green: deletes a previously saved order

--- src/tests/unit/domain/repositories.test.ts
+++ src/tests/unit/domain/repositories.test.ts
@@ -18,7 +18,7 @@ export class InMemoryOrderRepository implements OrderRepository {
     }
 
     async delete(id: Id): Promise<void> {
-        throw new Error("Method not implemented.");
+        this.orders = this.orders.filter(order => !order.getId().equals(id));
     }
 }
 
@@ -50,5 +50,20 @@ describe('The Order Repository', () => {
         const orders = await repository.findAll();
         expect(orders.length).toBe(1);
         expect(orders[0].toDto()).toEqual(order.toDto());
+    });
+
+    it('deletes a previously saved order', async () => {
+        const items = [
+            new OrderLine(Id.create(), PositiveNumber.create(2), PositiveNumber.create(3)),
+        ];
+        const address = Address.create("Irrelevant Street 123");
+        const order = Order.create(items, address);
+        const repository = new InMemoryOrderRepository();
+        await repository.save(order);
+
+        await repository.delete(order.getId());
+
+        const orders = await repository.findAll();
+        expect(orders.length).toBe(0);
     });
 });
